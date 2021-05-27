b75840335b079454a3653dd29f41b91dc4668105
rvrahulverma <rvrahulverma9921@gmail.com>
2021-05-28 00:52:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 26 12:37:13 2021
+
+@author: asus
+"""
+
+# #intigerVar
+# one=1
+# two=2
+
+# #Decimal or floatVar
+# decimal=1.1
+
+# #string
+# StringVar ="Hello"
+
+# print(one)
+# print(two)
+# print(decimal)
+# print(StringVar)
+
+#Function
+# def FunctionName():
+#     newVar="Hello World"
+#     print(newVar)
+#     return
+# FunctionName()
+     
+# # FirstWay 
+# def addOne(number):
+#     output = number + 1 
+#     print(output)
+#     return 
+# var=0
+# addOne(var)
+# #SecondWay
+# def plusOne(Number):
+#     output = Number+1
+#     return output
+# var=0
+# var2=plusOne(var)
+# print(var2)
+
+# More than one input
+# def addOneAddTwo(NumberOne,NumberTwo):
+#     output= NumberOne + 1
+#     output+= NumberTwo + 2
+#     return output 
+# sum=addOneAddTwo(1,2)
+# print(sum)
+    
+# def printmessage(message, ntimes = 1):
+#       print(message * ntimes)
+   
+# printmessage("hello", 5)
+    
+    
+# def artist():
+#       artist="arijit singh"
+#       print (artist) 
+# artist()      
+
+# click = False
+# Like = 0
+# click = True
+# if click == True:
+#     Like += 1
+#     click= False
+# print(Like)    
+
+# Temperature = 20
+# Thermo = 15
+# print(Thermo)
+
+# if Temperature <=15:
+#     Thermo += 5
+# print(Thermo)    
+
+# if Temperature >=20:
+#     Thermo -= 3
+# print(Thermo)    
+
+# Time = "Day"
+# Sleepy =  False
+# pajamas = "OFF"
+
+# if Time == "Night" and Sleepy == False :
+#     pajamas = "ON"
+# print(pajamas)    
+
+
+# Time = "Noon"
+# Sleepy = False
+# pajamas = "OFF"
+
+# if Time=="Night" or Sleepy==True:
+#     pajamas="ON"
+# elif Time== "Late night":
+#         pajamas="ON"
+# else:
+#     pajamas="Throw pajamas out of the window"        
+# print(pajamas)    
+    
+# def max(a, b):
+#      if a > b:
+#           print('a is the max')
+#  else:
+#       print('b is the max')
+
+# max(1,2)
+
+# def idealPC (Processor,RAM,HardDisk):
+#     if Processor=="i5" and RAM == 8 :
+#         print('True')
+#     elif  HardDisk=="SSD" and RAM==8 :
+#         print('True')
+#     else:
+#         print('Flase')
+# idealPC("i5",int(8.3),"HDD" )       
+
+#List
+
+Score = [70,70.8,80,95,85,74]
+print(Score)
+# print(Score[2:4])
+
+# Changing variable value
+# Score[2]=88
+# print(Score) 
+
+# Score[1:3]=[]
+# print(Score)
+# print(Score[2])
+
+#list under list
+# Score[2]=["Hello","World"]
+# print(Score)
+# print(Score[2][0])
+# Score[2][0]="Hey"
+# print(Score[2][0]) 
+# print(Score)   
+
+# Adding element to the list
+Score.append(35)
+print(Score)
+
+
+
+
+
+
+
+
+
+
